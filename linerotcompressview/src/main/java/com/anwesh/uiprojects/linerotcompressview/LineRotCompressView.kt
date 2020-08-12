170b2ffe2abdf12179f0b0abe024d6ff2cd9b09f
Anwesh43 <anweshthecool0@gmail.com>
2020-08-13 03:05:33 +0530

    adding constants and libraries

--- linerotcompressview/src/main/java/com/anwesh/uiprojects/linerotcompressview/LineRotCompressView.kt
+++ linerotcompressview/src/main/java/com/anwesh/uiprojects/linerotcompressview/LineRotCompressView.kt
@@ -3,3 +3,20 @@ package com.anwesh.uiprojects.linerotcompressview
 /**
  * Created by anweshmishra on 13/08/20.
  */
+
+import android.view.View
+import android.view.MotionEvent
+import android.content.Context
+import android.app.Activity
+import android.graphics.Canvas
+import android.graphics.Paint
+import android.graphics.RectF
+import android.graphics.Color
+
+val sizeFactor : Float = 5.6f
+val strokeFactor : Float = 90f
+val delay : Long = 20
+val parts : Int = 4
+val colors : Array<String> = arrayOf("#3F51B5", "#4CAF50", "#F44336", "#2196F3", "#FFEB3B")
+val scGap : Float = 0.02f / parts
+val backColor : Int = Color.parseColor("#BDBDBD")
