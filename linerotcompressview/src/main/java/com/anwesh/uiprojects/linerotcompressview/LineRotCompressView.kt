f1364f1fbeaa74087af76ab22144d5fb9d2ba68f
Anwesh43 <anweshthecool0@gmail.com>
2020-08-13 03:15:37 +0530

    adding LineRotCompressView class

--- linerotcompressview/src/main/java/com/anwesh/uiprojects/linerotcompressview/LineRotCompressView.kt
+++ linerotcompressview/src/main/java/com/anwesh/uiprojects/linerotcompressview/LineRotCompressView.kt
@@ -59,3 +59,19 @@ fun Canvas.drawLRCNode(i : Int, scale : Float, paint : Paint) {
     paint.strokeWidth = Math.min(w, h) / strokeFactor
     drawBiLineRotCompress(scale, w, h, paint)
 }
+
+class LineRotCompressView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
