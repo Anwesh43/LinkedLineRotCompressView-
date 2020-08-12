32f5be40e21a3b7afbc60cd89e11ffc83f5bccff
Anwesh43 <anweshthecool0@gmail.com>
2020-08-13 03:13:46 +0530

    adding Canvas extension methos

--- linerotcompressview/src/main/java/com/anwesh/uiprojects/linerotcompressview/LineRotCompressView.kt
+++ linerotcompressview/src/main/java/com/anwesh/uiprojects/linerotcompressview/LineRotCompressView.kt
@@ -25,3 +25,37 @@ fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
 fun Float.sinify() : Float = Math.sin(this * Math.PI).toFloat()
+
+
+fun Canvas.drawLineRotCompress(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val sf : Float = scale.sinify()
+    val sf1 : Float = sf.divideScale(0, parts)
+    val sf2 : Float = sf.divideScale(1, parts)
+    val sf3 : Float = sf.divideScale(2, parts)
+    val sf4 : Float = sf.divideScale(3, parts)
+    save()
+    translate(w / 10 + (w / 2 - w / 10) * sf2, 0f)
+    rotate(90f * sf3)
+    drawLine(0f, -size * sf1, 0f, size * sf1, paint)
+    drawRect(RectF(-size * sf4, -size, 0f, size), paint)
+    restore()
+}
+
+fun Canvas.drawBiLineRotCompress(scale : Float, w : Float, h : Float, paint : Paint) {
+    for (j in 0..1) {
+        save()
+        scale(1f - 2 * j, 1f)
+        drawLineRotCompress(scale, w, h, paint)
+        restore()
+    }
+}
+
+fun Canvas.drawLRCNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = Color.parseColor(colors[i])
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawBiLineRotCompress(scale, w, h, paint)
+}
