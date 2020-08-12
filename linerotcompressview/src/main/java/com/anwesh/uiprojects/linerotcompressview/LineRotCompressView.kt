acc01ab8633986bb0daf4e006ee82e952179bec4
Anwesh43 <anweshthecool0@gmail.com>
2020-08-13 03:07:44 +0530

    adding Int and Float extension methods

--- linerotcompressview/src/main/java/com/anwesh/uiprojects/linerotcompressview/LineRotCompressView.kt
+++ linerotcompressview/src/main/java/com/anwesh/uiprojects/linerotcompressview/LineRotCompressView.kt
@@ -20,3 +20,8 @@ val parts : Int = 4
 val colors : Array<String> = arrayOf("#3F51B5", "#4CAF50", "#F44336", "#2196F3", "#FFEB3B")
 val scGap : Float = 0.02f / parts
 val backColor : Int = Color.parseColor("#BDBDBD")
+
+fun Int.inverse() : Float = 1f / this
+fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
+fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+fun Float.sinify() : Float = Math.sin(this * Math.PI).toFloat()
